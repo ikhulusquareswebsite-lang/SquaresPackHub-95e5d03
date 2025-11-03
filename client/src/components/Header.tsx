340b152d8e686be9fd682db260705737d1a75499
ikhulusquareswebsite-lang <ikhulusquareswebsite@gmail.com>
2025-11-03 22:44:47 +0200

    Update Header.tsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button"
 import { ThemeToggle } from "./ui/theme-toggle"
 import { useAuth } from "@/contexts/AuthContext"
 import { useNavigate } from "react-router-dom"
-import logo from "@/assets/images/logo.jpg"
+import logo from "@/assets/images/logo.jpeg"
 
 export function Header() {
   const { logout } = useAuth()
